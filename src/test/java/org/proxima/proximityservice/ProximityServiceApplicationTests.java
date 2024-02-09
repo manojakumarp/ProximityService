3f68f4ce422632bd4bb79612e97ff97fa16e0537
manojakumarp <mpattanaik700@gmail.com>
2024-02-09 22:15:39 +0530

    initial repo setup

---
+++ src/test/java/org/proxima/proximityservice/ProximityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.proxima.proximityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
